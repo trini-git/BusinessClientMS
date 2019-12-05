ec8f2e85320c7b72ed32e25662b1f74165a3d608
U-USERSAD\ptrinida <ptrinida@LIM-JTZXMF2.usersad.everis.int>
2019-12-04 22:40:14 -0500

    Second microservice

---
+++ src/main/java/com/businessclient/BusinessClientMsApplication.java
@@ -0,0 +1,13 @@
+package com.businessclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessClientMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessClientMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/businessclient/BusinessClientMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.businessclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessClientMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
